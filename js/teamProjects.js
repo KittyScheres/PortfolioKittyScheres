4429dde8b8f71652db023765f4bae64ae4ed5cbd
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2023-12-27 15:23:00 +0100

    [ADDED] I added camp champ

---
+++ js/teamProjects.js
@@ -0,0 +1,15 @@
+var projects = [
+    {name: "Camp champ", description: "Be the champ of the camp.", imageURL: "test", project: "CampChamp"}
+];
+
+$(document).ready(function () {
+	projects.forEach(function (item) {
+        $("#projectList").append(
+            "<div>" +
+            "   <img>" + 
+            "   <h3>" + item.name + "</h3>" +
+            "   <p>" + item.description + "</p>" +
+            "</div>"
+        );
+    });
+});
