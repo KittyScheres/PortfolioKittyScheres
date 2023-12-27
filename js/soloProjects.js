f0693ed410135da1b671fe1e3d839f0a490d38b1
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2023-12-27 15:08:40 +0100

    [ADDED] I added a custom scroll bar

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,14 +1,37 @@
 var projects = [
-    {name: "Test", description: "Test", imageURL: "test", link: "Test"},
-    {name: "Test", description: "Test", imageURL: "test", link: "Test"},
-    {name: "Test", description: "Test", imageURL: "test", link: "Test"},
-    {name: "Test", description: "Test", imageURL: "test", link: "Test"}
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"},
+    {name: "Test", description: "Test", imageURL: "test", project: "Test"}
 ];
 
 $(document).ready(function () {
 	projects.forEach(function (item) {
         $("#projectList").append(
-            "<div>" +
+            "<div >" +
+            "   <img>" +
             "   <h3>" + item.name + "</h3>" +
             "   <p>" + item.description + "</p>" +
             "</div>"
