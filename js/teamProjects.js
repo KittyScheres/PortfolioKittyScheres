cbe5e86c4c5c6df52a03e64470029df849e2fa4e
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2024-01-29 10:14:17 +0100

    [ADDED] I added more games to the projects page

--- js/teamProjects.js
+++ js/teamProjects.js
@@ -1,11 +1,14 @@
 var projects = [
-    {name: "Camp champ", description: "Be the champ of the camp.", imageURL: "test", project: "CampChamp"}
+    {name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "test", project: "Ball"},
+    {name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "test", project: "FPSEngine"},
+    {name: "Camp champ", description: "A 2vs2 game of capture the flag made using the Unreal engine.", imageURL: "test", project: "CampChamp"},
+    {name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space", imageURL: "test", project: "Astrohaunt"},
 ];
 
 $("#projectList").ready(function () {
 	projects.forEach(function (item) {
         $("#projectList").append(
-            "<div>" +
+            "<div class=\"Game\" href=\"./index.html?game=" + item.project + "\">" +
             "   <img>" + 
             "   <h3>" + item.name + "</h3>" +
             "   <p>" + item.description + "</p>" +
