b7a65d36fbaa48b090aca0aa46a3d6dca4884718
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2024-01-29 10:25:19 +0100

    [CHANGED] Updated the projects content cards

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,15 +1,21 @@
 var projects = [
-    {name: "R-type", description: "This is a recreation of the clasic arcade game R-type.", imageURL: "test", project: "R-type"}
+    {visible: true, name: "R-type", description: "This is a recreation of the clasic arcade game R-type made using the C++ programming language.", imageURL: "test", project: "R-type"},
+    {visible: true, name: "LIME CPU raytracer", description: "A CPU based Raytracer made with the C++ programming language", imageURL: "test", project: "LIME"},
+    {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "test", project: "CSG"},
 ];
 
 $("#projectList").ready(function () {
 	projects.forEach(function (item) {
-        $("#projectList").append(
-            "<div>" +
-            "   <img>" +
-            "   <h3>" + item.name + "</h3>" +
-            "   <p>" + item.description + "</p>" +
-            "</div>"
-        );
+        if(visible) {
+            $("#projectList").append(
+                "<a href=\"./index.html?game=" + item.project + "\">" +
+                "   <div class=\"Game\">" +
+                "       <img>" +
+                "       <h3>" + item.name + "</h3>" +
+                "       <p>" + item.description + "</p>" +
+                "   </div>" +
+                "</a>"
+            );
+        }
     });
 });
--- js/teamProjects.js
+++ js/teamProjects.js
@@ -1,18 +1,22 @@
 var projects = [
-    {name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "test", project: "Ball"},
-    {name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "test", project: "FPSEngine"},
-    {name: "Camp champ", description: "A 2vs2 game of capture the flag made using the Unreal engine.", imageURL: "test", project: "CampChamp"},
-    {name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space", imageURL: "test", project: "Astrohaunt"},
+    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "test", project: "Ball"},
+    {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "test", project: "FPSEngine"},
+    {visible: false, name: "Camp champ", description: "A 2vs2 game of capture the flag made using the Unreal engine.", imageURL: "test", project: "CampChamp"},
+    {visible: false, name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space", imageURL: "test", project: "Astrohaunt"},
 ];
 
 $("#projectList").ready(function () {
 	projects.forEach(function (item) {
-        $("#projectList").append(
-            "<div class=\"Game\" href=\"./index.html?game=" + item.project + "\">" +
-            "   <img>" + 
-            "   <h3>" + item.name + "</h3>" +
-            "   <p>" + item.description + "</p>" +
-            "</div>"
-        );
+        if (visible){
+            $("#projectList").append(
+                "<a href=\"./index.html?game=" + item.project + "\">" +
+                "   <div class=\"Game\">" +
+                "       <img>" + 
+                "       <h3>" + item.name + "</h3>" +
+                "       <p>" + item.description + "</p>" +
+                "   </div>" +
+                "</a>"
+            );
+        }
     });
 });
