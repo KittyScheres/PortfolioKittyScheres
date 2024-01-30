27f7c669a911701658fa82b9a8514e1b7753b1ac
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2024-01-30 20:40:33 +0100

    [ADDED] I added thumbnails to the projets lists

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,7 +1,7 @@
 var projects = [
-    {visible: true, name: "R-type", description: "This is a recreation of the clasic arcade game R-type made using the C++ programming language.", imageURL: "test", project: "R-type"},
-    {visible: true, name: "LIME CPU raytracer", description: "A CPU based Raytracer made with the C++ programming language", imageURL: "test", project: "LIME"},
-    {visible: true, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "test", project: "CSG"},
+    {visible: true, name: "R-type", description: "This is a recreation of the clasic arcade game R-type made using the C++ programming language.", imageURL: "rtypeThumbnail.png", project: "R-type"},
+    {visible: true, name: "LIME CPU raytracer", description: "A CPU based Raytracer made with the C++ programming language.", imageURL: "LIMEThumbnail.png", project: "LIME"},
+    {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "BallThumbnail.png", project: "CSG"},
 ];
 
 $("#projectList").ready(function () {
@@ -12,10 +12,12 @@ $("#projectList").ready(function () {
             if(item.visible) {
                 $("#projectList").append(
                     "<a href=\"./index.html?page=soloProjects&game=" + item.project + "\">" +
-                    "   <div class=\"Game\">" +
-                    "       <img>" +
-                    "       <h3>" + item.name + "</h3>" +
-                    "       <p>" + item.description + "</p>" +
+                    "   <div class=\"game\">" +
+                    "       <img src=\"./images/thumbnails/" + item.imageURL + "\" />" +
+                    "       <div class=\"text\">" +
+                    "           <h3>" + item.name + "</h3>" +
+                    "           <p>" + item.description + "</p>" +
+                    "       </div>" +
                     "   </div>" +
                     "</a>"
                 );
--- js/teamProjects.js
+++ js/teamProjects.js
@@ -1,8 +1,8 @@
 var projects = [
-    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "test", project: "Ball"},
-    {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "test", project: "FPSEngine"},
-    {visible: true, name: "Camp champ", description: "A 2 vs 2 game of capture the flag made using the Unreal engine.", imageURL: "test", project: "CampChamp"},
-    {visible: true, name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space", imageURL: "test", project: "Astrohaunt"},
+    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "BallThumbnail.png", project: "Ball"},
+    {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "WombatCombatThumbnail.png", project: "FPSEngine"},
+    {visible: false, name: "Camp champ", description: "A 2 vs 2 game of capture the flag made using the Unreal engine.", imageURL: "BallThumbnail.png", project: "CampChamp"},
+    {visible: false, name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space.", imageURL: "BallThumbnail.png", project: "Astrohaunt"},
 ];
 
 $("#projectList").ready(function () {
@@ -13,10 +13,12 @@ $("#projectList").ready(function () {
             if (item.visible){
                 $("#projectList").append(
                     "<a href=\"./index.html?page=teamProjects&game=" + item.project + "\">" +
-                    "   <div class=\"Game\">" +
-                    "       <img>" + 
-                    "       <h3>" + item.name + "</h3>" +
-                    "       <p>" + item.description + "</p>" +
+                    "   <div class=\"game\">" +
+                    "       <img src=\"./images/thumbnails/" + item.imageURL + "\" />" +
+                    "       <div class=\"text\">" +
+                    "           <h3>" + item.name + "</h3>" +
+                    "           <p>" + item.description + "</p>" +
+                    "       </div>" +
                     "   </div>" +
                     "</a>"
                 );
