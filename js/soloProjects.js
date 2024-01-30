41a6de1892c8c78d286da1e25692c4336e49aadb
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2024-01-30 19:14:17 +0100

    [CHANGED] Updated the project template

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,7 +1,7 @@
 var projects = [
     {visible: true, name: "R-type", description: "This is a recreation of the clasic arcade game R-type made using the C++ programming language.", imageURL: "test", project: "R-type"},
     {visible: true, name: "LIME CPU raytracer", description: "A CPU based Raytracer made with the C++ programming language", imageURL: "test", project: "LIME"},
-    {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "test", project: "CSG"},
+    {visible: true, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "test", project: "CSG"},
 ];
 
 $("#projectList").ready(function () {
--- js/teamProjects.js
+++ js/teamProjects.js
@@ -1,8 +1,8 @@
 var projects = [
     {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "test", project: "Ball"},
     {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "test", project: "FPSEngine"},
-    {visible: false, name: "Camp champ", description: "A 2vs2 game of capture the flag made using the Unreal engine.", imageURL: "test", project: "CampChamp"},
-    {visible: false, name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space", imageURL: "test", project: "Astrohaunt"},
+    {visible: true, name: "Camp champ", description: "A 2 vs 2 game of capture the flag made using the Unreal engine.", imageURL: "test", project: "CampChamp"},
+    {visible: true, name: "Astrohaunt", description: "An Paradroid inspired game made with the Unreal engine where you play as an astronaut fighting ghosts in a haunted mansion in space", imageURL: "test", project: "Astrohaunt"},
 ];
 
 $("#projectList").ready(function () {
