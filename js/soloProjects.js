59f2edce791d035290c20da6434d11a16a90aa3e
Scheres,Ruan R.H.W <ruanscheres12@gmail.com>
2024-01-30 20:51:26 +0100

    .

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,6 +1,6 @@
 var projects = [
-    {visible: true, name: "R-type", description: "This is a recreation of the clasic arcade game R-type made using the C++ programming language.", imageURL: "rtypeThumbnail.png", project: "R-type"},
-    {visible: true, name: "LIME CPU raytracer", description: "A CPU based Raytracer made with the C++ programming language.", imageURL: "LIMEThumbnail.png", project: "LIME"},
+    {visible: false, name: "R-type", description: "This is a recreation of the clasic arcade game R-type made using the C++ programming language.", imageURL: "rtypeThumbnail.png", project: "R-type"},
+    {visible: false, name: "LIME CPU raytracer", description: "A CPU based Raytracer made with the C++ programming language.", imageURL: "LIMEThumbnail.png", project: "LIME"},
     {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "BallThumbnail.png", project: "CSG"},
 ];
 
