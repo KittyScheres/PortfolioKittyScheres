34e48fde699e87f56cec63016e2ba4c56921a3d1
Kitty Scheres <Kittyhwscheres@gmail.com>
2024-03-28 19:19:35 +0100

    [ADDED] I added a page for the LIME raytracer

--- js/teamProjects.js
+++ js/teamProjects.js
@@ -1,7 +1,7 @@
 var projects = [
-    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "BallThumbnail.png", project: "Ball"},
     {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "WombatCombatThumbnail.png", project: "FPSEngine"},
     {visible: false, name: "Camp champ", description: "A 2 vs 2 game of capture the flag made using the Unreal engine.", imageURL: "BallThumbnail.png", project: "CampChamp"},
+    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "BallThumbnail.png", project: "Ball"},
 ];
 
 $("#projectList").ready(function () {
