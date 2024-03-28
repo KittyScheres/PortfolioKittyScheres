745ab20d50433ded7d18554f35f35a94a75e044a
Kitty Scheres <Kittyhwscheres@gmail.com>
2024-03-28 19:24:48 +0100

    [FIXED] I fixed an issue with the thumbnail for the raytracer

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,6 +1,6 @@
 var projects = [
     {visible: true, name: "R-type", description: "A partial recreation of the R-type arcade game.", imageURL: "rtypethumbnail.png", project: "R-type"},
-    {visible: true, name: "Lime raytracer", description: "A CPU raytracer.", imageURL: "LIMEthumbnail.png", project: "Lime"},
+    {visible: true, name: "Lime raytracer", description: "A CPU raytracer.", imageURL: "LIMEThumbnail.png", project: "Lime"},
     {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "BallThumbnail.png", project: "CSG"},
     {visible: false, name: "[WIP] OpenXR project", description: "A project I created to learn about the OpenXR api for implementing VR/AR support.", imageURL: "BallThumbnail.png", project: "OpenXR"},
 ];
