14a53f93aff5badf7ca73d004a3ed06083b4db22
Kitty Scheres <Kittyhwscheres@gmail.com>
2024-03-31 13:15:40 +0200

    [ADDED] I added tags to the project list

--- js/soloProjects.js
+++ js/soloProjects.js
@@ -1,8 +1,8 @@
 var projects = [
-    {visible: true, name: "R-type", description: "A partial recreation of the R-type arcade game.", imageURL: "rtypethumbnail.png", project: "R-type"},
-    {visible: true, name: "Lime raytracer", description: "A CPU raytracer.", imageURL: "LIMEThumbnail.png", project: "Lime"},
-    {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", imageURL: "BallThumbnail.png", project: "CSG"},
-    {visible: false, name: "[WIP] OpenXR project", description: "A project I created to learn about the OpenXR api for implementing VR/AR support.", imageURL: "BallThumbnail.png", project: "OpenXR"},
+    {visible: true, name: "R-type", description: "A partial recreation of the R-type arcade game.", language: "C++", duration: "6 weeks", imageURL: "rtypethumbnail.png", project: "R-type"},
+    {visible: true, name: "Lime raytracer", description: "A CPU raytracer.", language: "C++", duration: "8 weeks", imageURL: "LIMEThumbnail.png", project: "Lime"},
+    {visible: false, name: "[WIP] Constructive Solid Geometry", description: "An implementation of constructive solid geometry with the C++ programming language.", language: "C++", duration: "Unknown", imageURL: "BallThumbnail.png", project: "CSG"},
+    {visible: false, name: "[WIP] OpenXR project", description: "A project I created to learn about the OpenXR api for implementing VR/AR support.", language: "C++", duration: "Unknown", imageURL: "BallThumbnail.png", project: "OpenXR"},
 ];
 
 $("#projectList").ready(function () {
@@ -18,6 +18,8 @@ $("#projectList").ready(function () {
                     "       <div class=\"text\">" +
                     "           <h3>" + item.name + "</h3>" +
                     "           <p>" + item.description + "</p>" +
+                    "           <p class=\"sameLine\"><b>Language:</b> " + item.language + "</p>" +
+                    "           <p class=\"sameLine\"><b>Duration:</b> " + item.duration + "</p>" +
                     "       </div>" +
                     "   </div>" +
                     "</a>"
--- js/teamProjects.js
+++ js/teamProjects.js
@@ -1,7 +1,7 @@
 var projects = [
-    {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", imageURL: "WombatCombatThumbnail.png", project: "FPSEngine"},
-    {visible: false, name: "Camp champ", description: "A 2 vs 2 game of capture the flag made using the Unreal engine.", imageURL: "CampChampThumbnail.png", project: "CampChamp"},
-    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", imageURL: "BallThumbnail.png", project: "Ball"},
+    {visible: true, name: "Wombat combat engine", description: "A custom FPS engine inspired by Quake.", language: "C++", duration: "16 weeks", teamSize: "7", imageURL: "WombatCombatThumbnail.png", project: "FPSEngine"},
+    {visible: false, name: "Camp champ", description: "A 2 vs 2 game of capture the flag made using the Unreal engine.", language: "Unreal blueprints", duration: "16 weeks", teamSize: "14", imageURL: "CampChampThumbnail.png", project: "CampChamp"},
+    {visible: true, name: "[WIP] Project Ball", description: "A custom engine based game inspired by the game \"Super Monkey Ball\" with a big focus on user generated content.", language: "C++", duration: "32 weeks", teamSize: "12", imageURL: "BallThumbnail.png", project: "Ball"},
 ];
 
 $("#projectList").ready(function () {
@@ -17,6 +17,9 @@ $("#projectList").ready(function () {
                     "       <div class=\"text\">" +
                     "           <h3>" + item.name + "</h3>" +
                     "           <p>" + item.description + "</p>" +
+                    "           <p class=\"sameLine\"><b>Language:</b> " + item.language + "</p>" +
+                    "           <p class=\"sameLine\"><b>Duration:</b> " + item.duration + "</p>" +
+                    "           <p class=\"sameLine\"><b>Team size:</b> " + item.teamSize + " people</p>" +
                     "       </div>" +
                     "   </div>" +
                     "</a>"
